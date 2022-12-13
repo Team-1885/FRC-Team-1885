6c4bab66b80fdf8cbf68f72b8c96d49075638653
Megan Farran <farranme23@pwcs-edu.org>
2022-12-12 19:22:28 -0500

    Worked on practice module

--- src/main/java/us/ilite/common/types/EIntakeData.java
+++ src/main/java/us/ilite/common/types/EIntakeData.java
@@ -4,7 +4,6 @@ import com.flybotix.hfr.codex.CodexOf;
 
 public enum EIntakeData implements CodexOf<Double> {
     ARM_STATE,
-
     ROLLER_VEL_ft_s,
     SET_ROLLER_VEL_ft_s,
     CURRENT_ROLLER_RPM,
--- src/main/java/us/ilite/robot/modules/PracticeSpinMotor.java
+++ src/main/java/us/ilite/robot/modules/PracticeSpinMotor.java
@@ -8,7 +8,6 @@ import us.ilite.common.types.EMatchMode;
 import us.ilite.robot.hardware.SparkMaxFactory;
 
 public class PracticeSpinMotor extends Module{
-    //private CANSparkMax mMotor;
     private TalonFX mMotor;
     public PracticeSpinMotor() {
         mMotor = new TalonFX(0); //change id to actual id: maybe? Settings.HW.CAN.kINRoller
@@ -16,16 +15,27 @@ public class PracticeSpinMotor extends Module{
 
     @Override
     public void modeInit(EMatchMode pMode){
-
+        //runs once when the robot is enabled
     }
 
     @Override
-    public void readInputs() {
+    public void readInputs() { // set current values that you get from your encoders
         db.intake.set(EIntakeData.ROLLER_PCT, mMotor.getMotorOutputPercent()); //maybe: mIntakeRoller.getSelectedSensorVelocity() * kScaledUnitsToRPM) / kMaxFalconSpeed
     }
 
-    public void setOutputs() {
-        double speed = db.intake.get(EIntakeData.DESIRED_ROLLER_pct);
-        mMotor.set(TalonFXControlMode.PercentOutput, speed);
+    public void setOutputs() { //set the hardware feature (in this case a motor) to your desiered value
+        //percent output example:
+        double percentSpeed = db.intake.get(EIntakeData.DESIRED_ROLLER_pct); // -1 -> 1
+        mMotor.set(TalonFXControlMode.PercentOutput, percentSpeed);
+
+        //velocity example:
+        double velocity = db.intake.get(EIntakeData.SET_ROLLER_VEL_ft_s); //feet per second
+        mMotor.set(TalonFXControlMode.Velocity, velocity);
+
     }
 }
+
+
+
+
+
