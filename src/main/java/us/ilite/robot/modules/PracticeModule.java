81020fac41070c81370a738760d06a32668aa722
katelyn ahn <katelyninhye@outlook.com>
2023-01-12 19:52:01 -0500

    started glass intake

--- src/main/java/us/ilite/robot/modules/PracticeModule.java
+++ src/main/java/us/ilite/robot/modules/PracticeModule.java
@@ -1,26 +1,32 @@
 package us.ilite.robot.modules;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import us.ilite.common.types.EIntakeData;
 import us.ilite.common.types.EMatchMode;
 import us.ilite.robot.hardware.TalonSRXFactory;
 import us.ilite.robot.modules.FalconDriveModule;
 
-public class PracticeModule  extends Module {
+public class PracticeModule extends Module {
 
+    private final NetworkTable mTable;
     private TalonFX mMotor;
     private double speed;
+    private String pEtntry;
 
 
-    public void PracticeModule() {
+    public PracticeModule() {
         TalonFX mMotor = new TalonFX(9);
-
+        mTable = NetworkTableInstance.getDefault().getTable("intake");
     }
 
     @Override
     public void modeInit(EMatchMode pMode){
 
     }
+
+
 
     @Override
     public void readInputs() {
@@ -31,8 +37,15 @@ public class PracticeModule  extends Module {
     @Override
     public void setOutputs() {
         mMotor.set(TalonFXControlMode.PercentOutput, db.intake.get(EIntakeData.DESIRED_ROLLER_pct));
+        
+        setNetworkTableValue("desiredpct", EIntakeData.DESIRED_ROLLER_pct);
+        setNetworkTableValue("rollerpct", EIntakeData.ROLLER_PCT);
 
     }
 
+    private void setNetworkTableValue(String pEntry, EIntakeData pEnum)
+    {
+        mTable.getEntry(pEtntry).setNumber(db.intake.get(pEnum));
+    }
 
 }
