fd1bdb05d90510b9e988838b544c673db02f0c4c
Michael Morales Reyes <MoraleMA25@pwcs-edu.org>
2023-01-14 08:39:29 -0500

    Added Glass to Pneumatic Class

--- src/main/java/us/ilite/robot/modules/PneumaticTest.java
+++ src/main/java/us/ilite/robot/modules/PneumaticTest.java
@@ -1,15 +1,20 @@
 package us.ilite.robot.modules;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import us.ilite.common.config.Settings;
+import us.ilite.common.types.EIntakeData;
 import us.ilite.common.types.EMatchMode;
 import us.ilite.robot.Enums;
 import static us.ilite.common.types.EClimberData.IS_SINGLE_CLAMPED;
 
 public class PneumaticTest extends Module {
-    private DoubleSolenoid mCLPNSingle;
+    private final DoubleSolenoid mCLPNSingle;
+    private final NetworkTable mTable;
     public PneumaticTest() {
         mCLPNSingle = new DoubleSolenoid(Settings.HW.PCH.kPCHCompressorModule, PneumaticsModuleType.REVPH, 4, 5);
+        mTable = NetworkTableInstance.getDefault().getTable("pneumatic");
     }
     @Override
     public void modeInit(EMatchMode mode) {
@@ -28,5 +33,6 @@ public class PneumaticTest extends Module {
         } else if (singleMode == Enums.EClampMode.RELEASED) {
             mCLPNSingle.set(DoubleSolenoid.Value.kReverse);
         }
+        mTable.getEntry("IS_SINGLE_CLAMPED").setNumber(db.climber.get(IS_SINGLE_CLAMPED));
     }
 }
