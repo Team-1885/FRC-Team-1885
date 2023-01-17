79c367874c64f8930e83fa1734ae5f9026347c6b
BigManSam1885 <LeeK26@pwcs-edu.org>
2023-01-17 16:26:15 -0500

    A_BTN for glass has no more errors

--- src/main/java/us/ilite/robot/modules/practice.java
+++ src/main/java/us/ilite/robot/modules/practice.java
@@ -8,6 +8,9 @@ import us.ilite.common.types.EIntakeData;
 import us.ilite.common.types.ELimelightData;
 import us.ilite.common.types.EMatchMode;
 import us.ilite.common.types.drive.EDriveData;
+import us.ilite.common.types.input.EDriverInputMode;
+import us.ilite.common.types.input.ELogitech310;
+
 import static us.ilite.common.types.ELimelightData.LED_MODE;
 import static us.ilite.common.types.ELimelightData.PIPELINE;
 
@@ -18,14 +21,17 @@ private double speed;
 
 public practice(){
     mMotor = new TalonFX(9);
-    mTable = NetworkTableInstance.getDefault().getTable("intake");
+    mIntakeTable = NetworkTableInstance.getDefault().getTable("intake");
+    mButtonTable = NetworkTableInstance.getDefault().getTable("button is pressed");
 }
 
 @Override
 public void modeInit(EMatchMode pMode){
 
 }
-    private final NetworkTable mTable;
+    private final NetworkTable mIntakeTable;
+
+    private final NetworkTable mButtonTable;
 
 @Override
 public void readInputs(){
@@ -37,14 +43,15 @@ public void setOutputs(){
     speed = db.drivetrain.get(EDriveData.L_ACTUAL_VEL_FT_s);
     mMotor.set(TalonFXControlMode.PercentOutput, db.intake.get(EIntakeData.DESIRED_ROLLER_pct));
     setIntakeTableValue("intake", EIntakeData.DESIRED_ROLLER_pct);
+    setButtonTableValue("button is pressed", ELogitech310.A_BTN);
 }
 
     private void setIntakeTableValue(String pEntry, EIntakeData pEnum) {
-        mTable.getEntry(pEntry).setNumber(db.intake.get(pEnum));
+        mIntakeTable.getEntry(pEntry).setNumber(db.intake.get(pEnum));
     }
 
-    private void setButtonTableValue(String pEntry, EIntakeData pEnum) {
-        mTable.getEntry(pEntry).setNumber(db.intake.get(pEnum));
+    private void setButtonTableValue(String pEntry, ELogitech310 pEnum) {
+        mButtonTable.getEntry(pEntry).setNumber(db.driverinput.get(pEnum));
     }
 
 }
