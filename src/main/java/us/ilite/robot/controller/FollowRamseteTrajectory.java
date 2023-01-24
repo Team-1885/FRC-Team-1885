26f4aa1a0ce202f2be7d8df227ea77357d9bb934
Megan Farran <farranme23@pwcs-edu.org>
2023-01-24 16:40:57 -0500

    WIP

--- src/main/java/us/ilite/robot/controller/FollowRamseteTrajectory.java
+++ src/main/java/us/ilite/robot/controller/FollowRamseteTrajectory.java
@@ -1,11 +1,21 @@
 package us.ilite.robot.controller;
 
+import edu.wpi.first.wpilibj.Timer;
+import us.ilite.robot.commands.FollowRamseteCommand;
+
 public class FollowRamseteTrajectory extends BaseAutonController{
 
-    
+    private FollowRamseteCommand mRamseteCommand;
+    private Timer mTimer;
+
 
     public void initialize() {
+        mRamseteCommand = new FollowRamseteCommand();
+        mTimer = new Timer();
+        mTimer.reset();
+        mTimer.start();
 
+        mRamseteCommand.init(mTimer.get());
     }
     public void updateImpl() {
 
