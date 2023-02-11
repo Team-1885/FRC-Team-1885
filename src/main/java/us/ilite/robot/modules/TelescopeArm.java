fd8ba9b2253844dddf68acd784c78d150485bcef
Samay Mahal <Mahals25@pwcs-edu.org>
2023-02-11 08:45:39 -0500

    commit to get the newest version

---
+++ src/main/java/us/ilite/robot/modules/TelescopeArm.java
@@ -0,0 +1,10 @@
+package us.ilite.robot.modules;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import us.ilite.common.config.Settings;
+import us.ilite.common.lib.control.PIDController;
+
+public class TelescopeArm extends Module{
+
+}
