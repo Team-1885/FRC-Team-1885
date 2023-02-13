072f246e8db2c867184df393d3f6e1651e52a85a
Megan Farran <farranme23@pwcs-edu.org>
2023-02-13 18:45:37 -0500

    continuing work on 's' curve traj

--- src/main/java/us/ilite/robot/commands/GenerateRamseteCommand.java
+++ src/main/java/us/ilite/robot/commands/GenerateRamseteCommand.java
@@ -68,10 +68,10 @@ public class GenerateRamseteCommand {
                 TrajectoryGenerator.generateTrajectory(
                         // Start at the origin facing the +X direction
                         new Pose2d(0, 0, new Rotation2d(0)),
-                        // Pass through these two interior waypoints, making an 's' curve path     
-                        List.of(new Translation2d(0.25, 0.25), new Translation2d(0.5, -0.25)),
+                        // Pass through these two interior waypoints, making an 's' curve path
+                        List.of(new Translation2d(1, 0)),
                         // End 3 meters straight ahead of where we started, facing forward
-                        new Pose2d(0.75, 0, new Rotation2d(0)),
+                        new Pose2d(2, 0, new Rotation2d(0)),
                         // Pass config
                         config);
 
