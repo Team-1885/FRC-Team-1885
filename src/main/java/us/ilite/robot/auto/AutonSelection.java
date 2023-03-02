d1d3696d94f72469bf4f13f442b4a6c6f0a95e22
Megan Farran <farranme23@pwcs-edu.org>
2023-03-02 17:49:40 -0500

    Revert "Merge branch 'divetrain'"
    This reverts commit 2f8d450619ce506024ae1bca2c28bb5bbacea58f.

--- src/main/java/us/ilite/robot/auto/AutonSelection.java
+++ src/main/java/us/ilite/robot/auto/AutonSelection.java
@@ -1,187 +1,63 @@
 package us.ilite.robot.auto;
 
-import com.pathplanner.lib.auto.RamseteAutoBuilder;
-import com.pathplanner.lib.commands.PPRamseteCommand;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import us.ilite.common.config.Settings;
-import com.pathplanner.lib.PathPlanner;
-import com.pathplanner.lib.PathPlannerTrajectory;
-import com.pathplanner.lib.PathConstraints;
-//import com.pathplanner.lib.PathPlanner;
-import us.ilite.robot.commands.GenerateRamseteCommand;
 import us.ilite.robot.controller.*;
-import us.ilite.robot.modules.NeoDriveModule;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 
 public class AutonSelection {
-    private RamseteController mRamseteController;
-
     public static ShuffleboardTab mAutonConfiguration = Shuffleboard.getTab("Pre-Match Configuration");
     public static int mDelaySeconds;
-    private SendableChooser<PPRamseteCommand> mSendableAutonControllers = new SendableChooser<>();
-    private PathPlannerTrajectory leftPiece;
-    private PathPlannerTrajectory leftOrigin;
-    private PathPlannerTrajectory mScoringAutomation;
-    private PathPlannerTrajectory mGoForward;
+    private SendableChooser<Class<?>> mSendableAutonControllers = new SendableChooser<>();
 
-    private PPRamseteCommand leftPieceCommand;
-    private PPRamseteCommand leftOriginCommand;
-    private PPRamseteCommand driveStraightCommand;
-    private PPRamseteCommand mScoringAutomationCommand;
-    private PPRamseteCommand mGoForwardCommand;
-
-
-    private PathPlannerTrajectory DriveStraight;
-    private PathPlannerTrajectory TurnTest;
-    private PathPlannerTrajectory Left;
-    private PathPlannerTrajectory Right;
-    private PathPlannerTrajectory CenterLeft;
-    private PathPlannerTrajectory CenterRight;
-    private SequentialCommandGroup mCommandGroup;
-//    private PathPlannerTrajectory
-    private int kMAX_ACCELERATON = 1;
-    private int kMAX_VELOCITY = 2;
-    private NeoDriveModule mDrive;
-    private PPRamseteCommand mRamseteCommand;
-    private NeoDriveModule mRobotDrive; // get singleton instance
-    private DifferentialDriveKinematics mDriveKinematics; // save instance kDriveKinematics for reuse
-    private PIDController mLeftDrivePID;
-    private PIDController mRightDrivePID;
-    private SimpleMotorFeedforward mFeedForward;
-    GenerateRamseteCommand commandGenerator;
-
-
-    public AutonSelection() {
-        commandGenerator = new GenerateRamseteCommand();
-        mDrive = NeoDriveModule.getInstance();
-        leftPiece = PathPlanner.loadPath("LeftPiece", new PathConstraints(kMAX_VELOCITY, kMAX_ACCELERATON));
-        leftOrigin = PathPlanner.loadPath("LeftOrigin", new PathConstraints(kMAX_VELOCITY, kMAX_ACCELERATON));
-        DriveStraight = PathPlanner.loadPath("DriveStraight", new PathConstraints(kMAX_VELOCITY, kMAX_ACCELERATON));
-        mScoringAutomation = PathPlanner.loadPath("ScoringAutomation", new PathConstraints(kMAX_VELOCITY, kMAX_ACCELERATON));
-        mGoForward = PathPlanner.loadPath("GoForward", new PathConstraints(kMAX_VELOCITY, kMAX_ACCELERATON));
-//        leftOrigin = PathPlanner.loadPath("LeftOrigin", new PathConstraints(kMAX_VELOCITY, kMAX_ACCELERATON));
-//        List<PathPlannerTrajectory> pathGroup1 = PathPlanner.loadPathGroup("CenterLeft", new PathConstraints(kMAX_VELOCITY, kMAX_ACCELERATON));
-
-        leftPieceCommand = commandGenerator.generateCommand(leftPiece);
-        leftOriginCommand = commandGenerator.generateCommand(leftOrigin);
-        driveStraightCommand = commandGenerator.generateCommand(DriveStraight);
-        mScoringAutomationCommand = commandGenerator.generateCommand(mScoringAutomation);
-        mGoForwardCommand = commandGenerator.generateCommand(mGoForward);
-//        RamseteAutoBuilder RAutoBuilder = new RamseteAutoBuilder(
-//                mRobotDrive::getPose,
-//                mRamseteController,
-//                mFeedForward,
-//                mDriveKinematics,
-//                mRobotDrive::getWheelSpeeds,
-//                mLeftDrivePID, // left controller
-//                mRightDrivePID, // right controller
-//                // RamseteCommand passes volts to the callback
-//                mRobotDrive::setVolts,
-//                mRobotDrive);
-//        RAutoBuilder.followPath(leftPiece);
-//        RAutoBuilder.followPath(leftOrigin);
-//        RAutoBuilder.followPathGroup(pathGroup1);
-
-//        leftPiece = new FollowTrajectory("LeftPiece");
-//        leftOrigin = new FollowTrajectory("LeftOrigin");
-//        DriveStraight = new FollowTrajectory("DriveStraight");
-//        TurnTest = new FollowTrajectory("TurnTest");
-//        Left = new FollowTrajectory("Left");
-//        Right = new FollowTrajectory("Right");
-//        CenterLeft = new FollowTrajectory("CenterLeft");
-//        CenterRight = new FollowTrajectory("CenterRight");
-//        mCommandGroup = new SequentialCommandGroup(leftPiece, leftOrigin, DriveStraight);
-
-        mSendableAutonControllers.addOption("left origin", leftOriginCommand);
-        mSendableAutonControllers.addOption("left piece", leftPieceCommand);
-        mSendableAutonControllers.addOption("drive straight", driveStraightCommand);
-        mSendableAutonControllers.addOption("auton selection", mScoringAutomationCommand);
-        mSendableAutonControllers.addOption("go fowradr", mGoForwardCommand);
-//        mSendableAutonControllers.addOption("path group", pathGroup1);
-//        mSendableAutonControllers.addOption("group", mCommandGroup);
-//        mSendableAutonControllers.addOption("TurnTest", TurnTest);
-//        mSendableAutonControllers.addOption("DriveStraight", DriveStraight);
-//        mSendableAutonControllers.addOption("LeftBall", Left);
-//        mSendableAutonControllers.addOption("RightBall", Right);
-//        mSendableAutonControllers.addOption("LeftToCharge", CenterLeft);
-//        mSendableAutonControllers.addOption("RightToCharge", CenterRight);
-        SmartDashboard.putData("Autonomous Mode", mSendableAutonControllers);
-        mRamseteController = new  RamseteController(
-                Settings.kRamseteB, // kRamseteB
-                Settings.kRamseteZeta // kRamseteZeta
-        );
-    }
-    public PPRamseteCommand getSelectedAutonController() {
-        return mSendableAutonControllers.getSelected();
-    }
-
-    public PathPlannerTrajectory getAutonTraj() {
-        return mScoringAutomation;
-    }
-
-//    private PathPlannerRamseteCommand generatePathPlannerRamseteCommand(PathPlannerTrajectory pPathPlannerTrajectory) {
-//        PathPlannerRamseteCommand command = new PathPlannerRamseteCommand (
-//            pPathPlannerTrajectory, mDrive::getPose, mRamseteController
-//        )
-//    }
-
-//    public AutonSelection() {
-//       mDelaySeconds = ((Double) (mAutonConfiguration.add("Path Delay Seconds", 0)
-//               .withPosition(2, 0)
-//               .withSize(2, 1)
-//               .getEntry()
-//               .getDouble(0.0)))
-//               .intValue();
-//
-//        mSendableAutonControllers.setDefaultOption("Default - Two ball", TwoBallController.class);
-//        for (Class<?> c : mAutonControllers) {
-//            mSendableAutonControllers.addOption(c.getSimpleName(), c);
-//        }
-//
-//        mAutonConfiguration.add("Choose Auton Controller", mSendableAutonControllers)
-//            .withPosition(3, 3)
-//            .withSize(4, 2);
-//    }
-//
-//    /**
-//     * Update these Auton Controllers whenever new ones are added
-//     */
-//    private Class<?>[] mAutonControllers = {
-//            ShootMoveController.class,
+    /**
+     * Update these Auton Controllers whenever new ones are added
+     */
+    private Class<?>[] mAutonControllers = {
+            ShootMoveController.class,
 //            TwoBallController.class,
 //            ThreeBallController.class,
 //            TexasSwitchController.class,
-////            FiveBallController.class,
-////            FourBallController.class,
-////            TwoBallTrajectoryController.class,
-////            FourBallTrajectoryAuton.class,
-////            ThreeBallTrajectoryController.class
-//    };
-//
-//    public BaseAutonController getSelectedAutonController() {
-//        try {
-//            return (BaseAutonController) mSendableAutonControllers.getSelected().getDeclaredConstructor().newInstance();
-//        } catch (NoSuchMethodException nsme) {
-//            nsme.printStackTrace();
-//        } catch (InstantiationException ie) {
-//            ie.printStackTrace();
-//        } catch (IllegalAccessException iae) {
-//            iae.printStackTrace();
-//        } catch (InvocationTargetException ite) {
-//            ite.printStackTrace();
-//        }
-//        // THIS SHOULD NEVER BE REACHED
-//        return null;
-//    }
+//            FiveBallController.class,
+//            FourBallController.class,
+//            TwoBallTrajectoryController.class,
+//            FourBallTrajectoryAuton.class,
+//            ThreeBallTrajectoryController.class
+    };
+
+    public AutonSelection() {
+       mDelaySeconds = ((Double) (mAutonConfiguration.add("Path Delay Seconds", 0)
+               .withPosition(2, 0)
+               .withSize(2, 1)
+               .getEntry()
+               .getDouble(0.0)))
+               .intValue();
+
+//        mSendableAutonControllers.setDefaultOption("Default - Two ball", TwoBallController.class);
+        for (Class<?> c : mAutonControllers) {
+            mSendableAutonControllers.addOption(c.getSimpleName(), c);
+        }
+
+        mAutonConfiguration.add("Choose Auton Controller", mSendableAutonControllers)
+            .withPosition(3, 3)
+            .withSize(4, 2);
+    }
+
+    public BaseAutonController getSelectedAutonController() {
+        try {
+            return (BaseAutonController) mSendableAutonControllers.getSelected().getDeclaredConstructor().newInstance();
+        } catch (NoSuchMethodException nsme) {
+            nsme.printStackTrace();
+        } catch (InstantiationException ie) {
+            ie.printStackTrace();
+        } catch (IllegalAccessException iae) {
+            iae.printStackTrace();
+        } catch (InvocationTargetException ite) {
+            ite.printStackTrace();
+        }
+        // THIS SHOULD NEVER BE REACHED
+        return null;
+    }
 }
