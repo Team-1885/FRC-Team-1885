873acd95b4cfae71808ff3764d4ebb4882c5576e
BigManSam1885 <LeeK26@pwcs-edu.org>
2023-03-11 14:43:34 -0500

    3/11/23 catching up to what I missed

--- src/main/java/us/ilite/robot/modules/AddressableLEDs.java
+++ src/main/java/us/ilite/robot/modules/AddressableLEDs.java
@@ -1,60 +1,75 @@
 package us.ilite.robot.modules;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import us.ilite.common.types.EMatchMode;
 
 public class AddressableLEDs extends Module{
+    private AddressableLED mLED;
+    private AddressableLEDBuffer mLEDBuffer;
+    private int mRainbowFirstPixelHue = 0;
+    private int mColorPalettePixelHueOne = 0;
+    private NetworkTable mTable;
+    int i = 0;
+    public AddressableLEDs() {
+        mLED = new AddressableLED(9);
+        mLEDBuffer = new AddressableLEDBuffer(60);
+        mLED.setLength(mLEDBuffer.getLength());
+        mTable = NetworkTableInstance.getDefault().getTable("newled");
+    }
     @Override
     public void modeInit(EMatchMode pMode) {
-        // PWM port 9
-        // Must be a PWM header, not MXP or DIO
-        AddressableLED m_led = new AddressableLED(9);
-
-        // Reuse buffer
-        // Default to a length of 60, start empty output
-        // Length is expensive to set, so only set it once, then just update data
-        AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(60);
-        m_led.setLength(m_ledBuffer.getLength());
-
-        // Set the data
-        m_led.setData(m_ledBuffer);
-        m_led.start();
-
-        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-            // Sets the specified LED to the HSV values for purple
-            m_ledBuffer.setHSV(i, 284, 100, 75);
-        }
-
-        m_led.setData(m_ledBuffer);
-
-        purpleOmbre();
 
     }
-
-    private void purpleOmbre() {
-        // For every pixel
-        AddressableLEDBuffer m_ledBuffer;
-        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-            // Calculate the hue - hue is easier for rainbows because the color
-            // shape is a circle so only one value needs to precess
-            final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
-            // Set the value
-            m_ledBuffer.setHSV(i, hue, 255, 128);
-        }
-        // Increase by to make the rainbow "move"
-        m_rainbowFirstPixelHue += 3;
-        // Check bounds
-        m_rainbowFirstPixelHue %= 180;
-    }
-
     @Override
     protected void readInputs() {
 
     }
     @Override
     protected void setOutputs() {
+        gamerColor();
+        mLED.setData(mLEDBuffer);
+        mLED.start();
+        //mTable.getEntry("RGBColor").setNumber();
+    }
+    /* private void gamerColor() {
+        // For every pixel
+        for ( var i = 0; i < mLEDBuffer.getLength(); i++) {
+            // Calculate the hue - hue is easier for rainbows because the color
+            // shape is a circle so only one value needs to precess
+            final var hue = (mRainbowFirstPixelHue + (i * 180 / mLEDBuffer.getLength())) % 180;
+            // Set the value
+            mLEDBuffer.setHSV(i, hue, 255, 128);
+        }
+        // Increase by to make the rainbow "move"
+        mRainbowFirstPixelHue += 3;
+        // Check bounds
+        mRainbowFirstPixelHue %= 180;
+    }
+     */
 
+    private void gamerColor() {
+        // For every pixel
+        if(i < 60){
+            // Calculate the hue - hue is easier for rainbows because the color
+            // shape is a circle so only one value needs to precess
+            final var colorPaletteHue = (mRainbowFirstPixelHue + (i * 135 / mLEDBuffer.getLength())) % 256;
+            // Set the value
+            mLEDBuffer.setHSV(i, colorPaletteHue, 30, 68);
+            i++;
+        }
+        else {
+            i = 0;
+            System.out.println("else statement");
+        }
+        // Increase by to make the rainbow "move"
+        mColorPalettePixelHueOne += 3;
+        // Check bounds
+        mColorPalettePixelHueOne %= 180;
     }
 }
+
 
