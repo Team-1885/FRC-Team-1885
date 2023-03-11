6c63c839e2d0424e756a932456e92afdaf97fa00
katelyn ahn <katelyninhye@outlook.com>
2023-03-11 09:55:54 -0500

    comet colors??
    commented out working rainbow led code and made a copy but with adjusted color values for our color palette- teal to purple
    going to test and see if it works

--- src/main/java/us/ilite/robot/modules/AddressableLEDs.java
+++ src/main/java/us/ilite/robot/modules/AddressableLEDs.java
@@ -11,6 +11,7 @@ public class AddressableLEDs extends Module{
     private AddressableLED mLED;
     private AddressableLEDBuffer mLEDBuffer;
     private int mRainbowFirstPixelHue = 0;
+    private int mColorPalettePixelHueOne = 0;
     private NetworkTable mTable;
     public AddressableLEDs() {
         mLED = new AddressableLED(9);
@@ -33,7 +34,7 @@ public class AddressableLEDs extends Module{
         mLED.start();
         //mTable.getEntry("RGBColor").setNumber();
     }
-    private void gamerColor() {
+    /* private void gamerColor() {
         // For every pixel
         for ( var i = 0; i < mLEDBuffer.getLength(); i++) {
             // Calculate the hue - hue is easier for rainbows because the color
@@ -47,19 +48,21 @@ public class AddressableLEDs extends Module{
         // Check bounds
         mRainbowFirstPixelHue %= 180;
     }
-    private void battlefieldColor() {
+     */
+
+    private void gamerColor() {
         // For every pixel
         for ( var i = 0; i < mLEDBuffer.getLength(); i++) {
             // Calculate the hue - hue is easier for rainbows because the color
             // shape is a circle so only one value needs to precess
-            final var hue = (mRainbowFirstPixelHue + (i * 180 / mLEDBuffer.getLength())) % 180;
+            final var colorPaletteHue = (mRainbowFirstPixelHue + (i * 135 / mLEDBuffer.getLength())) % 256;
             // Set the value
-            mLEDBuffer.setHSV(i, hue, 255, 128);
+            mLEDBuffer.setHSV(i, colorPaletteHue, 30, 68);
         }
         // Increase by to make the rainbow "move"
-        mRainbowFirstPixelHue += 3;
+        mColorPalettePixelHueOne += 3;
         // Check bounds
-        mRainbowFirstPixelHue %= 180;
+        mColorPalettePixelHueOne %= 180;
     }
 }
 
