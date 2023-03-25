a1545ae1e5792bffb5bc52674a859de204d38128
Samay Mahal <Mahals25@pwcs-edu.org>
2023-03-25 08:37:11 -0400

    made spinintake command

---
+++ src/main/java/us/ilite/robot/commands/SpinIntake.java
@@ -0,0 +1,25 @@
+package us.ilite.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class SpinIntake extends CommandBase {
+    @Override
+    public void initialize() {
+
+    }
+    @Override
+    public void execute() {
+
+    }
+    @Override
+    public boolean isFinished() {
+        if(something1 = something) {
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+    }
+}
